2dcf48f25e5a8e607baefa5a9085257138ebfbb9
Mark Steven Reyes <mstevenreyes@gmail.com>
2023-01-13 07:26:58 +0800

    Added layouts, css changes
    -removed most custom css from style.css and converted it to tailwind classes since it is more easy to maintain and customize
    -added project images for viewing of potential clients
    -changed font from Nunito Sans to Century Gothic
    - Removed Text beside Header Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
-            "./src/pages/**/*.{html,js}",
-            "./src/components/**/*.{html,js}"],
+  content: ['./index.html',
+            './src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
