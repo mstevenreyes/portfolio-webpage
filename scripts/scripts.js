2d6b334bb8675f5bf72c7d8fad5d202681a956d0
Mark Steven Reyes <mstevenreyes@gmail.com>
2023-05-16 04:56:58 +0800

    massive changes on layout/ui design
    - added new logo/favicon
    - new animation queries for navbar
    - added about me section,
    - spotlight mouse animation!

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// scrolling navbar anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener("click", function(e){
             e.preventDefault();
@@ -9,8 +10,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
 });
 
+// burger menu show/hide functionality
 const toggleButton = document.getElementById("burger-menu");
 const navBarLinks = document.getElementById("navbar-links");
 toggleButton.addEventListener("click", () => {
     navBarLinks.classList.toggle("active");
 });
+
+
+// mouse spotlight effect
+const main = document.querySelectorAll("body");
+main.forEach(main => {
+    main.addEventListener('mousemove', (e) => {
+        const rect = main.getBoundingClientRect();
+        const left = e.clientX - rect.left;
+        const top = e.clientY - rect.top;
+    
+        main.style.setProperty("--mouse-x", `${left}px`);
+        main.style.setProperty("--mouse-y", `${top}px`);
+    });
+})
