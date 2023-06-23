09fd665e2c23dae7965e3d13303be833922cf963
Mark Steven Reyes <mstevenreyes@gmail.com>
2023-06-23 15:35:35 +0800

    added Vite to list of Skills

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,6 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener("click", function(e){
             e.preventDefault();
-            console.log("test");
             var x = document.getElementById("dev-projects")
             document.querySelector(this.getAttribute("href")).scrollIntoView({
                 behavior : "smooth"
